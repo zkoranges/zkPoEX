a342b9b9da05c83e91b4a0bb6cbf433de8bba6b1
Maciej Zieliński <maciej@odra.dev>
2023-02-12 17:48:20 +0100

    Initial commit

---
+++ host/src/main.rs
@@ -0,0 +1,26 @@
+// TODO: Update the name of the method loaded by the prover. E.g., if the method is `multiply`, replace `METHOD_NAME_ID` with `MULTIPLY_ID` and replace `METHOD_NAME_PATH` with `MULTIPLY_PATH`
+use methods::{METHOD_NAME_ID, METHOD_NAME_PATH};
+use risc0_zkvm::Prover;
+// use risc0_zkvm::serde::{from_slice, to_vec};
+
+fn main() {
+    // Make the prover.
+    let method_code = std::fs::read(METHOD_NAME_PATH)
+        .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
+    let mut prover = Prover::new(&method_code, METHOD_NAME_ID).expect(
+        "Prover should be constructed from valid method source code and corresponding method ID",
+    );
+
+    // TODO: Implement communication with the guest here
+
+    // Run prover & generate receipt
+    let receipt = prover.run()
+        .expect("Code should be provable unless it 1) had an error or 2) overflowed the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
+
+    // Optional: Verify receipt to confirm that recipients will also be able to verify your receipt
+    receipt.verify(METHOD_NAME_ID).expect(
+        "Code you have proven should successfully verify; did you specify the correct method ID?",
+    );
+
+    // TODO: Implement code for transmitting or serializing the receipt for other parties to verify here
+}
---
+++ methods/guest/src/bin/method_name.rs
@@ -0,0 +1,10 @@
+// TODO: Rename this file to change the name of this method from METHOD_NAME
+
+#![no_main]
+#![no_std]  // std support is experimental, but you can remove this to try it
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    // TODO: Implement your guest code here
+}
