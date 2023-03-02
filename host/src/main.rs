d2d20e976f403baa03396ee3c3e93af1e5fd0e19
zkoranges <hangingoranges@proton.me>
2023-03-01 21:12:13 -0700

    fix: adapt risc0 proof to our data

--- host/src/main.rs
+++ host/src/main.rs
@@ -5,17 +5,17 @@ use risc0_zkvm::serde::{from_slice, to_vec};
 fn main() {
     println!("Proving state change after hack tx");
     let input = "e9be02aa";
-    let result:u32 = run_prover(input);
-    println!("Proof generated. Result = {result}");
+    let result:Vec<String> = run_prover(input);
+    println!("Proof generated. Result = {:?}", result);
 }
 
-fn run_prover(input: &str) -> u32 {
+fn run_prover(input: &str) -> Vec<String> {
     // Make the prover.
     let method_code = std::fs::read(EVM_CALC_PATH).unwrap();
     let mut prover = Prover::new(&method_code, EVM_CALC_ID).unwrap();
     prover.add_input_u32_slice(to_vec(input).unwrap().as_slice());
     let receipt = prover.run().unwrap();
     assert!(receipt.verify(EVM_CALC_ID).is_ok());
-    let result: String = from_slice(receipt.journal.as_slice()).unwrap();
-    u32::from_str_radix(&result, 16).unwrap()
+    let result: Vec<String> = from_slice(receipt.journal.as_slice()).unwrap();
+    result
 }
