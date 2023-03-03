1c5e070cd824cd3fa315e372f379e68763c9cc22
zkoranges <hangingoranges@proton.me>
2023-03-02 22:43:30 -0700

    feat: add new contracts to test

--- bytecode/Target.sol
+++ bytecode/Target.sol
@@ -1,16 +1,33 @@
-/// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+// DO NOT USE IN PRODUCTION
+// Ethernaut Lvl. 10 - Reentrance
 pragma solidity 0.8.19;
 
 contract Target {
-  uint256 public balance;
+  
+  mapping(address => uint) public balances;
 
-  function fund() external returns (uint256) {
-    balance = 100;
-    return balance;
+  function deposit(address _to) public payable {
+    balances[_to] = balances[_to] + msg.value;
   }
 
-function rug() external returns (uint256)  {
-    balance = 0;
-    return balance;
+  function balanceOf(address _who) public view returns (uint balance) {
+    return balances[_who];
   }
+
+  function withdraw(uint _amount) public {
+    if(balances[msg.sender] >= _amount) {
+      (bool result,) = msg.sender.call{value:_amount}("");
+      if(result) {
+        _amount;
+      }
+      unchecked {
+        balances[msg.sender] -= _amount;
+      }
+    }
+  }
+
+  constructor() payable {}
+
+  receive() external payable {}
 }
--- justfile
+++ justfile
@@ -5,8 +5,14 @@ compile-contract:
         --evm-version istanbul \
         --output-dir bytecode \
         bytecode/Target.sol  
+    solc --bin-runtime \
+        --optimize \
+        --overwrite \
+        --evm-version istanbul \
+        --output-dir bytecode \
+        bytecode/Exploiter.sol  
 
-test-evm-runtime: compile-contract
+test-evm: compile-contract
     cargo test -p evm-runner -- --nocapture
 
 prove: compile-contract
